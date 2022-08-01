9c81166a1d0f665832fb75f68768a2ef8680762f
jonh-dev <jonh.dev.br@gmail.com>
2022-08-01 18:12:01 -0300

    Finishing the create methods in Crud

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -7,6 +7,7 @@ import me.dio.academia.digital.service.impl.AlunoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,8 +18,8 @@ public class AlunoController {
     private AlunoServiceImpl service;
 
     @GetMapping
-    public List<Aluno> getAll(){
-       return service.getAll();
+    public List<Aluno> getAll(@RequestParam(value = "dataDeNascimento", required = false) String dataDeNascimento){
+       return service.getAll(dataDeNascimento);
     }
 
     @GetMapping("avaliacoes/{id}")
@@ -27,7 +28,7 @@ public class AlunoController {
     }
 
     @PostMapping()
-    public Aluno create(@RequestBody AlunoForm form){
+    public Aluno create(@Valid @RequestBody AlunoForm form){
         return service.create(form);
     }
 
--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -3,7 +3,9 @@ package me.dio.academia.digital.entity.form;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -11,11 +13,19 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class AlunoForm {
 
+  @NotBlank(message = "Preencha o campo com o seu nome.")
+  @Size(min = 3, max = 50, message = "$'{validateValue}' precisa ter entre {min} e {max} caracteres.")
   private String nome;
 
+  @NotBlank(message = "Preencha o campo com o seu CPF.")
+  @CPF(message = "'{validateValue}' é inválido.")
   private String cpf;
 
+  @NotBlank(message = "Preencha o campo com o seu bairro.")
+  @Size(min = 3, max = 50, message = "$'{validateValue}' precisa ter entre {min} e {max} caracteres.")
   private String bairro;
 
+  @NotNull(message = "Preencha o campo com sua data de nascimento.")
+  @Past(message = "Data '{validateValue}' é inválida.")
   private LocalDate dataDeNascimento;
 }
